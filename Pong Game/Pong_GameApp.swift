c6666cf6d499756cce685b5760e70bbdf9b8334e
Marek Michalski <marek.michalski@me.com>
2024-08-05 13:25:50 +0200

    Initial Commit

---
+++ Pong Game/Pong_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pong_GameApp.swift
+//  Pong Game
+//
+//  Created by Marek Michalski on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pong_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pong GameUITests/Pong_GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pong_GameUITestsLaunchTests.swift
+//  Pong GameUITests
+//
+//  Created by Marek Michalski on 05/08/2024.
+//
+
+import XCTest
+
+final class Pong_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
